db8788f98a1324399dbb7fb9def8eaf16e2e601d
Laura Weaver <laura.maclay.weaver@gmail.com>
2015-10-16 16:36:52 -0700

    [Close #8] Serves static files

--- server/server.py
+++ server/server.py
@@ -1,12 +1,11 @@
-from flask import Flask, request, send_from_directory
+from flask import Flask, request, send_from_directory, jsonify
 
 # set the project root directory as the static folder, you can set others.
-app = Flask(__name__, static_url_path='/..')
+app = Flask(__name__, static_url_path='')
 
-#serve index
 @app.route('/')
-def send_index():
-    return app.send_static_file('/client/index.html') 
+def send_js():
+    return send_from_directory('static', 'index.html')
 
 #serve client dependecies
 @app.route('/bower_components/<path:path>')
@@ -14,7 +13,6 @@ def send_dependencies(path):
     return send_from_directory('/client/bower_components', path)
 
 #handle api calls
-
 
 #start server
 if __name__ == "__main__":
