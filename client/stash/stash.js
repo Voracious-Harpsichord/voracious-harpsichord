ff017d93d06c72091ed0c6ba31aa9c95c86c0924
Cynthia Chen <checy16@gmail.com>
2015-10-16 16:58:04 -0700

    "Create stash folder with stash html and controller"

---
+++ client/stash/stash.js
@@ -0,0 +1,30 @@
+var stash = angular.module('beautystack.stash', []);
+
+stash.controller('StashController', ['$scope', 'Products', 'Spinner', 
+  function($scope, Products, Spinner) {
+    $scope.stash = {};
+
+    $scope.getAll = function() {
+      //Create and start spinner
+      var spinner = Spinner.create();
+      spinner.spin(document.querySelector('.stash-spinner'));
+
+      //Invoke entries factory get all method
+      Products.getAll()
+      .then(function(resp) {
+        spinner.stop();
+        $scope.products = resp;
+      })
+      .catch(function(err) {
+        console.log('Error with getting stack:', err);
+      });
+    };
+
+    $scope.addProduct = function() {
+      Products.add();
+      $scope.product = {};    
+      })
+    }
+
+
+  }]);
