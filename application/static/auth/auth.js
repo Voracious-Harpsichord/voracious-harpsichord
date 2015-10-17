f40fb570fd6086ebb8ae3782955f2e188830b159
Cynthia Chen <checy16@gmail.com>
2015-10-17 15:05:19 -0700

    "Add signup, signin, and signout functionality"

--- application/static/auth/auth.js
+++ application/static/auth/auth.js
@@ -1,21 +1,44 @@
 angular.module('beautystack.auth', [])
 
-.controller('AuthController', ['$scope', '$window', '$state', 'Auth', 'Spinner',
-  function($scope, $window, $state, Auth, Spinner) {
+.controller('AuthController', ['$scope', '$window', '$state', 'Auth'
+  function($scope, $window, $state, Auth) {
 
     $scope.user = {};
-    // $scope.name = $window.localStorage.getItem('beauty.username');
+    
+    $scope.signup = function() {
+      //Invoke signup function from Auth factory
+      Auth.signup($scope.user)
+      .then(function(userid) {
+        //Store userid in local storage
+        $window.localStorage.setItem('beauty.userid', userid);
+        //Transition to stash page
+        $state.transitionTo('stash');
+      })
+      .catch(function(error) {
+        console.error(error);
+        $scope.error = error.data;
+      });
+    };
 
     $scope.signin = function() {
-      //Create and start spinner
-      var spinner = Spinner.create();
-      spinner.spin(document.querySelector('.spinner'));
+      //Invoke signin function from Auth factory
+      Auth.signin($scope.user)
+      .then(function(userid) {
+        //Store userid in local storage
+        $window.localStorage.setItem('beauty.userid', userid);
+        //Transition to stash page
+        $state.transitionTo('stash');
+      })
+      .catch(function(error) {
+        console.error(error);
+        $scope.error = error.data;
+      });
+    };
 
+    $scope.signout = function() {
+      //Remove userid stored in local storage 
+      $window.localStorage.removeItem('beauty.userid', userid);
+      //Transition to home page
+      $state.transitionTo('home');
     }
-
-
-
-  }
-
-
-  ])
+}]);
