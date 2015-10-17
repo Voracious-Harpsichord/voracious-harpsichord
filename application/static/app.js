6aea9e041f1e3701ec24b1cb14ad8aa154e977bc
Cynthia Chen <checy16@gmail.com>
2015-10-17 15:03:31 -0700

    "Add authentication logic for requests for state changes"

--- application/static/app.js
+++ application/static/app.js
@@ -6,7 +6,6 @@ app.config(['$stateProvider', '$urlRouterProvider',
 
     $stateProvider
       .state('home', {
-        //Parent state of home; load home.html, set controller
         url: '/',
 
         views: {
@@ -98,4 +97,17 @@ app.config(['$stateProvider', '$urlRouterProvider',
 
       $urlRouterProvider.otherwise('/');
 
-  }]);
+  }])
+
+.run(function($rootScope, $location, $state, Auth) {
+  //Listens to state change and determines if user is authenticated
+  $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
+    //If state requires authentication and user is not authenticated
+    if (toState.data.requireLogin && !Auth.isAuth() {
+      //Prevent state transition from happening
+      event.preventDefault();
+      //Transition state to home page
+      $state.transitionTo('home');
+    })
+  })
+})
