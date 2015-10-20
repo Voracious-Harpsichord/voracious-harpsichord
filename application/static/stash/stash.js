7f71e8a005984f535a6a08f7d41322b8165f6e0c
Cynthia Chen <checy16@gmail.com>
2015-10-19 19:02:52 -0700

    "Debug methods on StashController. getAll and addProduct functionality works now!"

--- application/static/stash/stash.js
+++ application/static/stash/stash.js
@@ -1,27 +1,34 @@
 var stash = angular.module('beautystack.stash', []);
 
-stash.controller('StashController', ['$scope', 'Products',
-  function($scope, Products) {
-    $scope.products = {};
-    $scope.product = {};
+stash.controller('StashController',
+  function ($scope, Products) {
+    $scope.newProduct = '';
 
+    //Fetch all products in user's stash
     $scope.getAll = function() {
-
       //Invoke getAllProducts function from Products factory
       Products.getAllProducts()
       .then(function(resp) {
-        $scope.products = resp;
-        console.log('Fetching products');
+        $scope.products = resp.data;
       })
-      .catch(function(err) {
-        console.error('Error with getting products:', err);
+      .catch(function(error) {
+        console.error('Error with getting products:', error);
       });
     };
 
+    //Add a product 
     $scope.addProduct = function() {
-      Products.addProduct(JSON.stringify($scope.product));
-      console.log('Product added');
-      $scope.product = {};
+      var product = {'brand_name': 'Sephora', 'product_name': $scope.newProduct};
+      Products.addProduct(product)
+      .then(function(resp) {
+        $scope.newProduct = '';
+        $scope.products.push(resp.data);
+        console.log('scope.products:', $scope.products);
+      })
+      .catch(function(error) {
+        console.error('Error with adding product:', error);
+      });
     };
-  }
-]);
+
+    $scope.getAll();
+  });
