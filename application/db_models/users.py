d8f010527d37354a96e9e581f0a99162296c8ebb
jtknox91 <jtknox91@gmail.com>
2015-10-21 16:58:56 -0700

    Expanded but User table but untested

--- application/db_models/users.py
+++ application/db_models/users.py
@@ -2,12 +2,28 @@ from server import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    created_at =db.Column(db.Date)
     username = db.Column(db.String(80), unique=True)
     pw_hash = db.Column(db.String(120))
+    email = db.Column(db.String(80), unique=True)
+    name_title = db.Column(db.String(80))
+    name_first = db.Column(db.String(80))
+    name_last = db.Column(db.String(80))
+    gender = db.Column(db.String(80))
+    location = db.Column(db.String(80))
+    birthday = db.Column(db.Date)
     
-    def __init__(self, username, pw_hash):
+    def __init__(self, created_at, username, pw_hash, email='', name_title='', name_first='', name_last='', gender='', location='', birthday=0):
+        self.created_at = created_at
         self.username = username
         self.pw_hash = pw_hash
+        self.email = email
+        self.name_title = name_title
+        self.name_first = name_first
+        self.name_last = name_last
+        self.gender = gender
+        self.location = location
+        self.birthday = birthday
 
     def __repr__(self):
         return '<User %r>' % self.username
