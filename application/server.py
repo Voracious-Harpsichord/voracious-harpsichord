65e8fad99df2ca8f0224f22b0a664719be7f104e
jtknox91 <jtknox91@gmail.com>
2015-10-21 19:03:06 -0700

    [Close #89, close #86] Expanded user table and respond with complete user object

--- application/server.py
+++ application/server.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, send_from_directory, jsonify
 from flask.ext.bower import Bower
 from flask.ext.bcrypt import Bcrypt
-import datetime
 # set the project root directory as the static folder, you can set others.
 app = Flask(__name__, static_url_path='')
 
@@ -35,7 +34,8 @@ def user():
     #if user has correct password
     if u_ctrl.verify_user(body['username'], body['password']):
         #make response
-        response = jsonify({'userid': u_ctrl.get_user_id(body['username'])})
+        user_id = u_ctrl.get_user_id(body['username'])
+        response = jsonify(u_ctrl.get_user_as_dictionary(user_id))
         #add session-cookie to response
         u_ctrl.create_session(response)
         #return user object with a 200
@@ -50,12 +50,13 @@ def newUser():
     #if user is not already in db
     if not u_ctrl.user_exists(body['username']):
         #add user to db
-        u_ctrl.make_new_user(str(datetime.datetime.now()), body.get('username'), body.get('password'), body.get('email'), body.get('name_title'), body.get('name_first'), body.get('name_last'), body.get('gender'), body.get('location'), body.get('birthday'), )
+        u_ctrl.make_new_user(body)
         #make response
-        response = jsonify({'userid': u_ctrl.get_user_id(body['username'])})
-        #add session-cooker to response
+        user_id = u_ctrl.get_user_id(body['username'])
+        response = jsonify(u_ctrl.get_user_as_dictionary(user_id))
+        #add session-cookie to response
         u_ctrl.create_session(response)
-        #return user object witha 201
+        #return user object with a 201
         return response, 201
     #else return a 302 for Found
     else:
