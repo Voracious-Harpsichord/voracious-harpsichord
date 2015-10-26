b37640263f3168839df57b1ebeceb3549bcbcff8
Laura Weaver <laura.maclay.weaver@gmail.com>
2015-10-26 11:38:55 -0700

    Refactor database and controllers; add functionality to frontend

--- application/db_models/products.py
+++ application/db_models/products.py
@@ -2,14 +2,22 @@ from server import db
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    product_name = db.Column(db.String(120))
-    product_brand = db.Column(db.String(80))
-    product_category = db.Column(db.String(20))
+    name = db.Column(db.String(120))
+    brand = db.Column(db.String(80))
+    category = db.Column(db.String(20))
+    price = db.Column(db.Integer)
+    sephora_id = db.Column(db.String(20))
+    image_url = db.Column(db.String(240))
+    description = db.Column(db.String(500))
 
-    def __init__(self, product_name, product_brand, product_category):
-        self.product_name = product_name
-        self.product_brand = product_brand
-        self.product_category = product_category
+    def __init__(self, name, brand, category, price, sephora_id, image_url, description):
+        self.name = name
+        self.brand = brand
+        self.category = category
+        self.price = price
+        self.sephora_id = sephora_id
+        self.image_url = image_url
+        self.description = description
 
     def __repr__(self):
         return '<product %r>' % self.product_name
@@ -19,18 +27,24 @@ class User_product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer)
     product_id = db.Column(db.Integer)
-    product_size = db.Column(db.String(20))
-    product_status = db.Column(db.String(20))
-    product_notes = db.Column(db.String(500))
-    product_color = db.Column(db.String(40))
+    size = db.Column(db.String(20))
+    status = db.Column(db.String(20))
+    notes = db.Column(db.String(500))
+    color = db.Column(db.String(40))
+    stars = db.Column(db.Integer)
+    review = db.Column(db.String(500))
+    user_product_image_url = db.Column(db.String(240))
 
-    def __init__(self, user_id, product_id, product_size, product_status, product_notes, product_color):
+    def __init__(self, user_id, product_id, size, status, notes, color, stars, review, user_product_image_url):
         self.user_id = user_id
         self.product_id = product_id
-        self.product_size = product_size
-        self.product_status = product_status
-        self.product_notes = product_notes
-        self.product_color = product_color
+        self.size = size
+        self.status = status
+        self.notes = notes
+        self.color = color
+        self.stars = stars
+        self.review = review
+        self.user_product_image_url = user_product_image_url
 
     def __repr__(self):
         return '<product_id %r>' % self.product_id
