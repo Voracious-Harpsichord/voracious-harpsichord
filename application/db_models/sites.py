5710ca25c24cdfb4ce7173bc15baa84d20fc4e3b
jtknox91 <jtknox91@gmail.com>
2015-10-29 15:45:03 -0700

    new PR

--- application/db_models/sites.py
+++ application/db_models/sites.py
@@ -1,5 +1,25 @@
 from server import db
 
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    site_name = db.Column(db.String(120)) #Name of publicaction ie. NY times
+    article_name = db.Column(db.String(120)) #Name of specific article
+    author_name = db.Column(db.String(60))
+    url = db.Column(db.String(500))
+    img = db.Column(db.String(500))
+    description = db.Column(db.String(500))
+
+    def __init__(self, site_name, article_name, author_name, url, img, description):
+        self.site_name = site_name
+        self.article_name = article_name
+        self.author_name = author_name
+        self.url = url
+        self.img = img
+        self.article = description
+
+    def __repr__(self):
+        return '<article %r>' % self.article_name
+
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     blog_name = db.Column(db.String(120))
@@ -14,17 +34,7 @@ class Blog(db.Model):
         self.article = article
 
     def __repr__(self):
-        return '<site %r>' % self.site_name
-
-class Article(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    site_name = db.Column(db.String(120)) #Name of publicaction ie. NY times
-    article_name = db.Column(db.String(120)) #Name of specific article
-    author_name = db.Column(db.String(60))
-    url = db.Column(db.String(500))
-    img = db.Column(db.String(500))
-    description = db.Column(db.String(500))
-
+        return '<blog %r>' % self.blog_name
 
 class User_site(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -33,9 +43,10 @@ class User_site(db.Model):
     site_type = db.Column(db.String(60))
     comment = db.Column(db.String(500))
 
-    def __init__(self, user_id, site_id, comment):
+    def __init__(self, user_id, site_id, site_type, comment):
         self.user_id = user_id
         self.site_id = site_id
+        self.site_type = site_type
         self.comment = comment
 
     def __repr__(self):
