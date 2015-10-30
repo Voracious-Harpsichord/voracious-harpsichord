016c5aa7bec0f435d1da06a682ac1969abe3e1bd
Mike <michalpso@gmail.com>
2015-10-30 10:55:57 -0700

    added controller and model

--- application/db_controller/recommendation_controller.py
+++ application/db_controller/recommendation_controller.py
@@ -13,6 +13,7 @@ from db_models.users import User,Recommendation
 def get_recommendation_by_user_id(user_id):
     return session.query(Recommendation).filter(Recommendation.user_id == user_id).all()
 
+# 
 def add_recommendation(user_id, product_id, rank):
     recommendation = Recommendation(int(user_id),int(product_id),int(rank))
     session.add(product)
@@ -22,9 +23,9 @@ def add_recommendation(user_id, product_id, rank):
         'product_id':int(user_id),
         'rank':int(rank)
     }
-    
+
 #Delete all recommendations a user has
-def remove_user_from_product(product_id):
-    session.delete(session.query(User_product).filter(User_product.id == product_id).all())
+def remove_recommendation(user_id):
+    session.delete(session.query(Recommendation).filter(Recommendation.user_id == user_id).all())
     session.commit()
     return None
