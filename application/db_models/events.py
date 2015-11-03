9d79e12f11b54bf8758dfcdc83e2fd11d1172a7d
Laura Weaver <laura.maclay.weaver@gmail.com>
2015-11-02 17:28:38 -0800

    [Close #198, Close #139]Feeds

--- application/db_models/events.py
+++ application/db_models/events.py
@@ -3,10 +3,10 @@ from server import db
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer) #The user who added a thing
-    action = db.Column(db.String(40)) #..."added an article"
-    view_type = db.Column(db.String(40)) #the type of thing that was added ie product, blog
+    action = db.Column(db.String(140)) #..."added an article"
+    view_type = db.Column(db.String(140)) #the type of thing that was added ie product, blog
     data_id = db.Column(db.Integer) #the id for the type of thing that was added, to retrive more info about it
-    time_stamp = db.Column(db.String(40)) #when the action was performed
+    time_stamp = db.Column(db.String(140)) #when the action was performed
 
     def __init__(self, user_id, action, view_type, data_id, time_stamp):
         self.user_id = user_id
